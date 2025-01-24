4883e44d99d420e755ab90f0fe953598bd0590f4
Sunil Mishra <sunil_1905@outlook.com>
2025-01-24 13:02:21 +0530

    week 4 python container deploy

--- main.tf
+++ main.tf
@@ -1,196 +1,23 @@
-#Banckend configuration to store the tfstate file 
-
-terraform {
-  backend "s3" {
-    bucket = "web-s3-tfstate-bucket"
-    key    = "myproject/dev/terraform.tfstate"
-    encrypt        = true
-    region = "ap-southeast-2"
-  }
-}
-#providers details
 provider "aws" {
-  region     = "ap-southeast-2"
-  
+  region = "ap-southeast-2"
 }
-# VPC and Networking
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
 
+resource "aws_instance" "node_app" {
+  ami           = "ami-003f5a76758516d1e" # Ubuntu Server 20.04 LTS
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-06890c2f98c212144"
+ 
   tags = {
-    Name = "main-vpc"
+    Name = "NodeAppInstance"
   }
+  user_data = "IyEvYmluL2Jhc2gKc3VkbyBhcHQgdXBkYXRlIC15CnN1ZG8gYXB0IGluc3RhbGwgbmdpbnggLXkKc3VkbyBzeXN0ZW1jdGwgc3RhcnQgbmdpbngKc3VkbyBzeXN0ZW1jdGwgZW5hYmxlIG5naW54"
+
+    key_name = "Web1-Key_pair" # Replace with your key pair name
 }
 
-resource "aws_subnet" "public_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_1_cidr
-  availability_zone = var.availability_zone_1
-
-  tags = {
-    Name = "public-subnet-1"
-  }
-}
-
-resource "aws_subnet" "public_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_2_cidr
-  availability_zone = var.availability_zone_2
-
-  tags = {
-    Name = "public-subnet-2"
-  }
-}
-
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main-igw"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "public-route-table"
-  }
-}
-
-resource "aws_route_table_association" "public_1" {
-  subnet_id      = aws_subnet.public_1.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "public_2" {
-  subnet_id      = aws_subnet.public_2.id
-  route_table_id = aws_route_table.public.id
-}
-
-# Load Balancer and Target Group
-resource "aws_lb" "web" {
-  name               = "web-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
-
-  tags = {
-    Name = "web-alb"
-  }
-}
-
-resource "aws_lb_target_group" "web" {
-  name     = "web-target-group"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-
-  health_check {
-    path                = "/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-    matcher             = "200-299"
-  }
-
-  tags = {
-    Name = "web-target-group"
-  }
-}
-
-resource "aws_lb_listener" "web" {
-  load_balancer_arn = aws_lb.web.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.web.arn
-  }
-}
-
-# Launch Template with Nginx Installation
-resource "aws_launch_template" "web" {
-  name          = "web-launch-template"
-  image_id      = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
- user_data = "IyEvYmluL2Jhc2gKc3VkbyBhcHQgdXBkYXRlIC15CnN1ZG8gYXB0IGluc3RhbGwgbmdpbnggLXkKc3VkbyBzeXN0ZW1jdGwgc3RhcnQgbmdpbngKc3VkbyBzeXN0ZW1jdGwgZW5hYmxlIG5naW54"
-
-  network_interfaces {
-    associate_public_ip_address = true
-    security_groups             = [aws_security_group.web_sg.id]
-  }
-
-  tags = {
-    Name = "Web-CI-CD"
-  }
-}
-
-# Auto Scaling Group and Attachment
-resource "aws_autoscaling_group" "web" {
-  desired_capacity     = 2
-  max_size             = 3
-  min_size             = 1
-  vpc_zone_identifier  = [aws_subnet.public_1.id, aws_subnet.public_2.id]
-  launch_template {
-    id      = aws_launch_template.web.id
-    version = "$Latest"
-  }
-
-  tag {
-    key                 = "Name"
-    value               = "WebServerASG"
-    propagate_at_launch = true
-  }
-}
-
-resource "aws_autoscaling_attachment" "asg_alb" {
-  autoscaling_group_name = aws_autoscaling_group.web.name
-  lb_target_group_arn    = aws_lb_target_group.web.arn
-}
-
-# Security Groups
-resource "aws_security_group" "lb_sg" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "lb-sg"
-  }
-}
-
-resource "aws_security_group" "web_sg" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    security_groups = [aws_security_group.lb_sg.id]
-  }
+resource "aws_security_group" "node_app_sg" {
+  name        = "node_app_sg"
+  description = "Allow SSH and HTTP traffic"
 
   ingress {
     from_port   = 22
@@ -199,20 +26,21 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "web-sg"
-  }
 }
 
-# Output the DNS name of the Load Balancer
-output "load_balancer_dns_name" {
-  description = "The DNS name of the load balancer"
-  value       = aws_lb.web.dns_name
+output "instance_id" {
+  value = aws_instance.node_app.id
 }
--- terraform.tfvars
+++
@@ -1,8 +0,0 @@
-ami_id               = "ami-003f5a76758516d1e" #"ami-0abcdef1234567890"
-instance_type        = "t2.micro"
-key_name          = "Web1-Key_pair"
-vpc_cidr             = "10.0.0.0/16"
-public_subnet_1_cidr = "10.0.1.0/24"
-public_subnet_2_cidr = "10.0.2.0/24"
-availability_zone_1  = "ap-southeast-2a"
-availability_zone_2  = "ap-southeast-2b"
